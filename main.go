1eaf1d28ecc8adfa3738bedb4b7c7ef5de895e26
Icosatess <icosatess@example.com>
2024-10-23 23:38:13 -0400

    Replace app token with user token
    That is to say, 3-legged OAuth.

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"strings"
 
-	"golang.org/x/oauth2/clientcredentials"
+	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/twitch"
 )
 
@@ -22,50 +23,83 @@ type sendChatMessageRequestBody struct {
 
 func main() {
 	secrets := GetSecrets()
-	cfg := clientcredentials.Config{
+	conf := &oauth2.Config{
 		ClientID:     secrets.ClientID,
 		ClientSecret: secrets.ClientSecret,
-		TokenURL:     twitch.Endpoint.TokenURL,
 		Scopes: []string{
-			"user:write:chat",
-			"user:bot",
-			"channel:bot",
+			"user:read:chat", "user:write:chat",
 		},
+		Endpoint:    twitch.Endpoint,
+		RedirectURL: "http://localhost:8082",
 	}
 
-	client := cfg.Client(context.Background())
-
-	broadcasterID, botID := GetBotsUserID(client, secrets.ClientID)
-
-	messageBody := sendChatMessageRequestBody{
-		BroadcasterID: broadcasterID,
-		SenderID:      botID,
-		Message:       "Hello, world!",
+	url := conf.AuthCodeURL("", oauth2.AccessTypeOffline)
+	if _, err := fmt.Printf("Go to auth URL: %s", url); err != nil {
+		panic(err)
 	}
 
-	jreq, jreqErr := json.Marshal(messageBody)
-	if jreqErr != nil {
-		panic(jreqErr)
+	done := make(chan any)
+	var callbackServer *http.Server
+
+	oauthCallbackHandler := func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("OAuth callback handler invoked")
+		code := r.FormValue("code")
+
+		go func(code string) {
+			log.Printf("Entered newly spawned goroutine")
+			// Will all of this run as the server is shutting down?
+			tok, tokErr := conf.Exchange(context.TODO(), code)
+			if tokErr != nil {
+				panic(tokErr)
+			}
+
+			client := conf.Client(context.TODO(), tok)
+
+			broadcasterID, botID := GetBotsUserID(client, secrets.ClientID)
+
+			messageBody := sendChatMessageRequestBody{
+				BroadcasterID: broadcasterID,
+				SenderID:      botID,
+				Message:       "Hello, world!",
+			}
+
+			jreq, jreqErr := json.Marshal(messageBody)
+			if jreqErr != nil {
+				panic(jreqErr)
+			}
+
+			req, reqErr := http.NewRequest(http.MethodPost, "https://api.twitch.tv/helix/chat/messages", bytes.NewBuffer(jreq))
+			if reqErr != nil {
+				panic(reqErr)
+			}
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Client-Id", secrets.ClientID)
+			resp, respErr := client.Do(req)
+			if respErr != nil {
+				panic(respErr)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != 200 {
+				log.Printf("Wanted status code 200, but was %d", resp.StatusCode)
+				bs, _ := io.ReadAll(resp.Body)
+				log.Printf("Response body was %s", string(bs))
+				panic(resp)
+			}
+
+			SubscribeForUpdates()
+			done <- nil
+		}(code)
+
+		callbackServer.Shutdown(context.TODO())
 	}
 
-	req, reqErr := http.NewRequest(http.MethodPost, "https://api.twitch.tv/helix/chat/messages", bytes.NewBuffer(jreq))
-	if reqErr != nil {
-		panic(reqErr)
+	callbackServer = &http.Server{
+		Addr:    "127.0.0.1:8082",
+		Handler: http.HandlerFunc(oauthCallbackHandler),
 	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Client-Id", secrets.ClientID)
-	resp, respErr := client.Do(req)
-	if respErr != nil {
-		panic(respErr)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		log.Printf("Wanted status code 200, but was %d", resp.StatusCode)
-		bs, _ := io.ReadAll(resp.Body)
-		log.Printf("Response body was %s", string(bs))
-		panic(resp)
-	}
+	callbackServer.ListenAndServe()
+	<-done
 }
 
 type getUsersResponseBody struct {
