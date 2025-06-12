1c7cddf0359c3ebe40bd7821f6890af678c863b8
Icosatess <icosatess@example.com>
2025-06-11 23:59:14 -0400

    Replace top-level Panicfs with Fatalfs
    since there can't be any even higher level recover()s

--- main.go
+++ main.go
@@ -22,9 +22,9 @@ func main() {
 	if errors.Is(connErr, errCouldNotSubscribe) {
 		log.Printf("failed to subscribe for listening to chat: %v", connErr)
 	} else if errors.Is(connErr, errUnexpectedProtocolInteraction) {
-		log.Panicf("failed to subscribe for listening to chat due to unexpected interaction with Twitch: %v", connErr)
+		log.Fatalf("failed to subscribe for listening to chat due to unexpected interaction with Twitch: %v", connErr)
 	} else if connErr != nil {
-		log.Panicf("failed to subscribe to chat due to unexpected error: %v", connErr)
+		log.Fatalf("failed to subscribe to chat due to unexpected error: %v", connErr)
 	}
 	defer conn.Close(websocket.StatusNormalClosure, "Bye")
 
